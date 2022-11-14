68ba928e4963c2a4ac98a38bca85011b5e11c6a0
Daniela Lopez <dlope0831@gmail.com>
2022-11-13 21:56:22 -0600

    added dist folder, webpack, loaders

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static("../client/dist/"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('../client/'));
 
 require('./routes/htmlRoutes.js')(app);
 
