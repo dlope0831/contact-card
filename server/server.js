4234596ed431c9b3d706c68a7e6dc50638936023
Daniela Lopez <dlope0831@gmail.com>
2022-11-15 19:58:02 -0600

    added changes

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static("../client/dist/"))
+app.use(express.static("../client/dist"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes.js')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
