763923bc69049f07b80f27f77eb576b7b0ec4f8e
Daniela Lopez <dlope0831@gmail.com>
2022-11-17 17:14:44 -0600

    added indexdb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,19 +2,30 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb } from './database';
+import {initdb, postDb, deleteDb, editDb} from './database';
+import {fetchCards} from './cards';
+import { toggleForm, clearForm } from './form';
 
+// Import CSS files
+import "../css/index.css";
 
+// Import Bootstrap
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// On load functionality
 window.addEventListener('load', function () {
-    initdb();
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+  initdb();
+  fetchCards();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
 
-    // Import CSS files
-    import "../css/index.css";
+
+
